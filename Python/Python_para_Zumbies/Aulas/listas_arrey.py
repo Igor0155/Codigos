0be617f867ecb4d5555bf63a65aa4a4e1b99cba9
igorGabriel <igorgabriel0155@gmail.com>
2021-04-17 02:06:11 -0300

    Atualizando

---
+++ Python/Python_para_Zumbies/Aulas/listas_arrey.py
@@ -0,0 +1,9 @@
+ap = ["Andar 1",
+      "Andar 2",
+      "Andar 3",
+      "Andar 4"]
+
+print(ap[0])
+print(ap[1])
+print(ap[2])
+print(ap[3])
--- Python/Python_para_Zumbies/Lista_Exercicio_3/notas.py
+++
@@ -1,9 +0,0 @@
-aux = 0
-
-while aux == 0:
-    notas = float(input('\nInsira as notas <0 - 10 >: '))
-
-    if notas < 0 or notas > 10:
-        print('\nValor invalido')
-    else:
-        aux = 1
