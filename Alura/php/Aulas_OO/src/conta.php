b6ed9282ccb6964a7685ce08fdf886bfb1812429
igorGabriel <igorgabriel0155@gmail.com>
2021-10-06 18:10:07 -0300

    Uploads das aulas de php Orientado a Objeto

--- Alura/php/Aulas_OO/src/conta.php
+++ Alura/php/Aulas_OO/src/conta.php
@@ -1,11 +1,12 @@
 <?php
 
-function criarConta(string $cpf, string $nomeTitular, float $saldo): array
+
+// -> Acessa um atributo desse objeto 
+
+class Conta 
 {
-    return [
-        $cpf => [
-            'titular' => $nomeTitular,
-            'saldo' => $saldo,
-        ]
-    ];
+    // defir dados da conta e ja definindo o tipo da variavel(string,float,int)
+    public string $cpfTitular ;
+    public string $nomeTitular;
+    public float $saldo;
 }
